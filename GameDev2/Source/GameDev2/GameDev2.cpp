4967633f76352fc182610eb4d03afd7418a19fa7
Paul Steiner <thefarkinator@gmail.com>
2016-04-06 10:41:57 -0400

    For some reason the project will not build now... Gonna go over the DE.ini file in class. Meanwhile, getting lobby system working.

---
+++ GameDev2/Source/GameDev2/GameDev2.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GameDev2.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameDev2, "GameDev2" );
